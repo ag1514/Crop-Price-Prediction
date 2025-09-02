9a6106e11f587cdeaca085ae35181ea3ab2aa5ea
Ansh Goyal <ag717910@gmail.com>
2025-09-03 00:12:16 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
+
+const userSchema = mongoose.Schema({
+    username: String,
+    name: String,
+    password: String,
+    email: String,
+    age: Number,
+    profilepic:{
+        type: String,
+        default: "defaultpic.png"
+    },
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post"
+    }]
+})
+
+module.exports = mongoose.model('user', userSchema);
